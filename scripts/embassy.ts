0ea69a58cd0a6d67a2d1c470a8211b24d871eed6
J H <2364004+Blu-J@users.noreply.github.com>
2024-02-01 12:07:40 -0700

    chore: An example of using the demo specific code

--- scripts/embassy.ts
+++ scripts/embassy.ts
@@ -3,3 +3,7 @@ export { getConfig } from "./procedures/getConfig.ts";
 export { properties } from "./procedures/properties.ts";
 export { migration } from "./procedures/migrations.ts";
 export { main } from "./procedures/main.ts";
+
+export const jsMain = async ({ effects, utils, mainId }: any) => {
+  return utils.daemonRun(mainId, ["/usr/local/bin/docker_entrypoint.sh"], {});
+};
