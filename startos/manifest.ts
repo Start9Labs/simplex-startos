99d9f5e7679227e74fb64db186ea027fafa26f3b
Mariusz Kogen <k0gen@pm.me>
2025-09-23 18:00:48 +0200

    update for latest Makefile, sdk.beta41 and devDeps

--- startos/manifest.ts
+++ startos/manifest.ts
@@ -16,7 +16,7 @@ export const manifest = setupManifest({
   supportSite: 'https://github.com/simplex-chat/simplexmq/issues',
   marketingSite: 'https://simplex.chat/',
     docsUrl:
-    'https://github.com/Start9Labs/simplex-startos/blob/master/instructions.md',
+    'https://github.com/Start9Labs/simplex-startos/blob/update/040/docs/README.md',
   donationUrl:
     'https://github.com/simplex-chat/simplex-chat#help-us-with-donations',
   // @TODO
