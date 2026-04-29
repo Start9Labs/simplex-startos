ad9834ff7722dc57cb4c075727dacd302cfe3163
Aiden McClelland <me@drbonez.dev>
2026-04-28 23:06:31 -0600

    Bump version to 6.4.5:6

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,7 +1,8 @@
 import { VersionGraph } from '@start9labs/start-sdk'
 import { v_6_4_5_5 } from './v6.4.5.5'
+import { v_6_4_5_6 } from './v6.4.5.6'
 
 export const versionGraph = VersionGraph.of({
-  current: v_6_4_5_5,
-  other: [],
+  current: v_6_4_5_6,
+  other: [v_6_4_5_5],
 })
---
+++ startos/versions/v6.4.5.6.ts
@@ -0,0 +1,16 @@
+import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
+
+export const v_6_4_5_6 = VersionInfo.of({
+  version: '6.4.5:6',
+  releaseNotes: {
+    en_US: 'Internal updates (start-sdk 1.3.3)',
+    es_ES: 'Actualizaciones internas (start-sdk 1.3.3)',
+    de_DE: 'Interne Aktualisierungen (start-sdk 1.3.3)',
+    pl_PL: 'Aktualizacje wewnętrzne (start-sdk 1.3.3)',
+    fr_FR: 'Mises à jour internes (start-sdk 1.3.3)',
+  },
+  migrations: {
+    up: async ({ effects }) => {},
+    down: IMPOSSIBLE,
+  },
+})
